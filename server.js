40fc4de8bd5cc0e4aec823c12d20271e0b6e189d
jalonghurst <jacqui.longhurst@btinternet.com>
2020-12-24 20:34:39 +0000

    adding files

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const { reset } = require('nodemon');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex'); 
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+  // Enter your own database information here based on what you created
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'jacquelinelonghurst',
+    password : '',
+    database : 'smartbrain'
+  }
+});
+
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {res.send('success') })
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req,res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req,res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req,res) => {image.handleApiCall(req, res)})
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+})
+
+// ENDPOINTS TO WORK ON
+
+// /route route that res = this is working
+// /signin route - that is a post request, posting user info, res = success /or fail
+// /register will be a POST request to add the data to the database/a variable in the server with new user info. post = user
+// /profile/:userId --> GET = user
