c7251a31138f0ef70704b14b7bfcca63ef6fd6ff
jalonghurst <jacqui.longhurst@btinternet.com>
2020-12-27 17:29:31 +0000

    changing host

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const db = knex({
   // Enter your own database information here based on what you created
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'jacquelinelonghurst',
-    password : '',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
